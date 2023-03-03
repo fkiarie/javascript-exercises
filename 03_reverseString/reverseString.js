8359d3ccd8da3e36387da10b5ec965b400a5fe53
fkiarie <kazungu61@outlook.com>
2023-03-03 20:04:26 +0700

    a reverse string function
    Takes in a string, makes it an array, reverse the array,
    join the reversed array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let stringArray = string.split(""); // split the string and make it an array
+    const revArray= stringArray.reverse(); // reverse the array
+    const results = revArray.join(""); // join the reversed array
+    return results;
+
+
 
 };
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
