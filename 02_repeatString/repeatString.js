1cef0f94aecb4fec7dd31c5a39c4cc3a76ff781e
fkiarie <kazungu61@outlook.com>
2023-03-03 15:07:09 +0700

    Repaet a string at given times
    Included a function that repeats a given string as
    per given times. The test check if the strings is printed
    as per given times, if the string can return error when
    given a negative number and also return an empty string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let results = '';
+    if(num>=0){
+        for (let i = 0; i < num; i++) {
+            results += string;
+        }
+        return results;
+    }else{
+        return "ERROR";
+    }
+   
 
 };
 
+
+//console.log(repeatString('hey',5));
 // Do not edit below this line
 module.exports = repeatString;
