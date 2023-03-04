28281295e0cb2580166b6432b0f877a8c19d5ef1
fkiarie <kazungu61@outlook.com>
2023-03-04 11:17:40 +0700

    remove from array
    the function takes in a array as an argurment and other
    arguments. the loop goes through the array removing elements
    that match the args given

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (mainArray, ...args) {
+   // assign the array in argument to a newArray
+    let newArray = [...mainArray]; 
+     // loop through the arg checking for given element to delete
+   for(let i=0; i < args.length; i++){ 
+   // loop the args removing the matching element. 
+    newArray = newArray.filter(element => element !== args[i]); 
+   }
+   return newArray;
 
 };
-
+const anArray = [3,4,5,6,7,8,9];
+console.log(removeFromArray(anArray, 9));
 // Do not edit below this line
 module.exports = removeFromArray;
