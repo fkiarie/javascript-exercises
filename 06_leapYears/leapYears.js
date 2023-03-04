76cf2d920fadb2a9ba682c49cb6d7147247ab651
fkiarie <kazungu61@outlook.com>
2023-03-04 16:03:20 +0700

    check for leap Years
    the function checks if a year is divisible 4
    and not divisible by 100 which is a leap year
    if is also divisble 400, it is a leap year else
    false

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // check if the year is divisble by 4 and not divisble by 100 but also divisble by 400
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+        return true
+    }else{
+        return false;
+    }
+    };
+    console.log(leapYears(2004));
+    // Do not edit below this line
+    module.exports = leapYears;
 
-};
 
-// Do not edit below this line
-module.exports = leapYears;
+// leap year are divisble 4
+// if year is divisble by 100, then NOT leap year
+// unless divisible by 400
