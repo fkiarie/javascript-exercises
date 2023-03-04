f3b2366ab510ca18a483013fa40fe4a88a506dd9
fkiarie <kazungu61@outlook.com>
2023-03-04 19:36:09 +0700

    temperature converter function
    Two function, one converts celsius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInput) {
+
+  const c = (tempInput - 32) * 5 / 9;
+  let celsius = c.toFixed(1);
+  return parseFloat(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInput) {
+  const f = (tempInput * 9 / 5) + 32;
+  let fahrenheit = f.toFixed(1);
+  return parseFloat(fahrenheit);
 };
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(100));
 
 // Do not edit below this line
 module.exports = {
