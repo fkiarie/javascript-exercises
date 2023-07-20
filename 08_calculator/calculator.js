45d76f8ac0e0b04450fbaa1490b948d7e52f12ba
kazungu <kazungu61@outlook.com>
2023-07-20 13:31:32 +0300

    define the functions
    define the fucnctionality for add, subtract, sum up arrays, power and
    factorial of a number

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	const result = a+b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	const result = a-b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+ return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+return array.reduce((acc, current) => acc * current);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 1 || n === 0){
+    return 1;
+  }else{
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
