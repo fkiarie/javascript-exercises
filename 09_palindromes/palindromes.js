3f416af463d5b8780a2184bb5a9df7f24207222c
kazungu <kazungu61@outlook.com>
2023-07-20 15:34:25 +0300

    a palindromes function
    removes special characters, then splits, reverses the letters and joins
    then checks if the reversed is same as the inital word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    return cleanedWord == reversedWord;
 };
 
 // Do not edit below this line
