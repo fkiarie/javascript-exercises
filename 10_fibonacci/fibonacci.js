faa2c9649b1351094915f75076716d68926ed50c
kazungu <kazungu61@outlook.com>
2023-07-20 20:22:29 +0300

    fibonaccifunction logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(number) {
+    if (number <= 0) {
+      return "OOPS";
+    }else if(number === 0){
+        return 0;
+    }
+    let a = 0;
+    let b = 1;
+    for(let i = 1; i < number; i++){
+        const temp = b;
+        b= a+b;
+        a = temp;
+    }
+    return b;
+  };
+  
+  // Test the function
+  const n = 4;
+  console.log(`The ${n}th Fibonacci number is: ${fibonacci(n)}`);
+  
+  
 // Do not edit below this line
 module.exports = fibonacci;
