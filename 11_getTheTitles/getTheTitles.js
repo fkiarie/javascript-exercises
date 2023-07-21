ef69dfe1ec25205e8f781c99a721789aad066ddb
kazungu <kazungu61@outlook.com>
2023-07-21 17:15:13 +0300

    get the titles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    // Empty array to store the titles
+    const titles = [];
+  
+    // Loop through the books array and extract the titles
+    for (const book of books) {
+      titles.push(book.title);
+    }
+  
+    return titles;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
